011dbf07ed387acebdc18a525bf84da53d2840a4
Hiro-1978 <hiromiuki11@gmail.com>
2021-06-17 09:52:22 +0700

    User Bootstrap
    User Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductItemComponent } from './productitem/productitem.component';
 import { ProductListComponent } from './productlist/productlist.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductItemComponent,
-    ProductListComponent
+    ProductListComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule
